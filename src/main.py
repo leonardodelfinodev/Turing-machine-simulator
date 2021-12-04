edd1ac2c16609e956f43a2cc916724e403623386
leonardo-delfino <leonardo.delfino.2000@gmail.com>
2021-12-04 20:35:09 +0100

    Implemented the parsing of the input

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from utils import parse_input as parser
 
 def main():
-    pass
+    instructions = parser.parse_input_file("../examples/example0.txt")
+    if type(instructions) == "int":
+        print("ok")
+    print(instructions)
 
 if __name__ == '__main__':
     main()
--- src/utils/parse_input.py
+++ src/utils/parse_input.py
@@ -1,13 +1,24 @@
-# given an input file, extract the instructions into a list of 5-tuple
+import numpy as np
+import re
+from pathlib import Path
+
+def nonblank_lines(f):
+    for l in f:
+        line = l.rstrip()
+        if line:
+            yield line
+
+# given an input file, extracts the instructions into a list of 5-tuple
 def parse_input_file(file):
-    # check if the file exists
+    if not Path(file).is_file():
+        raise IOError("the file does NOT exist.")
+    # check if the tuple is valid (any, any, any, any, > or <) -> ignores if there are any errors in the instructions
+    regexp = re.compile("\(\s?([^\s]+),\s?([^\s]+),\s?([^\s]+),\s?([^\s]+),\s?([<>])\)")
+    instructions = []
+    # every symbol outside the brackets (on same row) will be ignored
     with open(file, "r") as f:
-        # TODO:
-        # for each line in the file, check if the 5-tuple is valid (regexp?)
-        # if it is, insert it into a numpy array
-        # if it's not, return an error
-
-        # every symbol outside the brackets (same row) will be ignored
-
-        # skip empty lines
-        pass
+        for line in nonblank_lines(f):
+            if regexp.search(line) is None:
+                raise Exception("the instruction {0} contains an error.".format(line))
+            instructions.append([op.lstrip(' ') for op in line[1:-1].split(",")])
+    return np.array(instructions)
